d8e61a90b720f87b133ce7b2e6b6472fd860a9da
Илья Цой <ilvits@192-168-0-146.local>
2021-07-11 13:41:38 +0300

    errors

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('post/new/', views.post_new, name='post_new'),
     path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
 ]
